15883f97c58655a10887cc32867c04a0d0f94a5b
Rajan Midun Magar <43414436+rajanmidun@users.noreply.github.com>
2020-06-10 17:07:39 +0545

    Add files via upload

---
+++ Part 4.js
@@ -0,0 +1,10 @@
+//Find all the pair of integer array whose sum is equal to given number
+// 12
+const values = [13, 2, 3, 4, 10, 5, 6, 7, 9, 11];
+
+for (let i = 0; i < values.length; i++) {
+  for (let j = i + 1; j < values.length; j++) {
+    if (values[i] + values[j] == 15)
+      console.log(`Pair of numbers whose sum is equal to 12 are ${values[i]}, ${values[j]}`);
+  }
+}
