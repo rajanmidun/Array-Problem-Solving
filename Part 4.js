cd02a05444f496adcf36a5b3d3b41e223d8ce3b9
Rajan Midun Magar <43414436+rajanmidun@users.noreply.github.com>
2020-06-10 17:08:16 +0545

    Update Part 4.js

--- Part 4.js
+++ Part 4.js
@@ -1,5 +1,5 @@
 //Find all the pair of integer array whose sum is equal to given number
-// if given number=12
+// if given number=15
 const values = [13, 2, 3, 4, 10, 5, 6, 7, 9, 11];
 
 for (let i = 0; i < values.length; i++) {
